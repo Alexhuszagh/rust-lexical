a3d4873b71817cdbb0a9662ef06d9badd3b9c3ca
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2018-11-24 19:00:59 -0600

    Bug fix for quickcheck without the precise algorithm.

--- src/ftoa/api.rs
+++ src/ftoa/api.rs
@@ -331,6 +331,7 @@ mod tests {
         }
     }
 
+    #[cfg(all(feature = "table", not(feature = "imprecise")))]
     quickcheck! {
         fn f32_quickcheck(f: f32) -> bool {
             f == atof32_bytes(10, f32toa_bytes(f, 10).as_slice())
