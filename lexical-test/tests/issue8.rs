e5295a17709cd3b1ab0bbf494228de690b780d40
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2019-02-13 14:15:33 -0600

    Bug fix for #8, Rust apparently debug checks for pointers to be null when explicitly creating an uninitialized pointer, one that is written to but never read.

---
+++ lexical-test/tests/issue8.rs
@@ -0,0 +1,19 @@
+extern crate lexical;
+
+#[test]
+fn vector_issue() {
+    let mut results = Vec::<f32>::new();
+    let line_split_vec = vec!["1", "2", "3"];
+    results.extend({
+          line_split_vec.iter().map(|x| {
+                lexical::try_parse::<f32, _>(x.trim()).unwrap()
+           })
+     });
+    assert_eq!(results, vec![1f32, 2f32, 3f32]);
+}
+
+#[test]
+fn float_test() {
+    let num: f64 = lexical::try_parse(b"5.002868148396374").unwrap();
+    assert_eq!(num, 5.002868148396374);
+}
