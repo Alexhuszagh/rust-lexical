911514894a5d9b94dfbf3c52203654aa3e99a617
Alex Huszagh <ahuszagh@gmail.com>
2020-01-06 19:32:16 -0600

    Removed unnecessary build dependencies.

--- lexical-core/build.rs
+++ lexical-core/build.rs
@@ -1,18 +1,4 @@
-extern crate rustc_version;
-
-fn version_parse(version: &str) -> rustc_version::Version {
-    rustc_version::Version::parse(version).unwrap()
-}
-
 fn main() {
-    let version = rustc_version::version().unwrap();
-
-    // VERSIONS
-    // --------
-
-    // We need at minimum version 1.37.0.
-    assert!(version >= version_parse("1.37.0"));
-
     // TARGET
     // ------
 
