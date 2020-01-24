0ca8813f75ec7372b5fc9b44d0c292513a7563c2
Alex Huszagh <ahuszagh@gmail.com>
2020-01-23 23:41:06 -0600

    Updated fuzz targets.

--- lexical-core/fuzz/fuzz_targets/atou16.rs
+++ lexical-core/fuzz/fuzz_targets/atou16.rs
@@ -2,6 +2,13 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate lexical_core;
 
+#[cfg(not(feature = "format"))]
 fuzz_target!(|data: &[u8]| {
-    let _ = lexical_core::atou16(data);
+    let _ = lexical_core::parse::<u16>(data);
 });
+
+#[cfg(feature = "format")]
+fuzz_target!(|data: &[u8]| {
+    let _ = lexical_core::parse_format::<u16>(data, lexical_core::NumberFormat::OCAML_STRING);
+});
+
