dd82a3fb6f013d8b28eb3fac63c91fece9edaa43
Alex Huszagh <ahuszagh@gmail.com>
2021-04-19 10:51:56 -0500

    Fix build.rs for #43.
    Default to 32-bit limbs if CARGO_CFG_TARGET_ARCH is not set. This way,
    we do not rely on the host architecture for the fallback. 32-bit limbs
    should generally be performant on most architectures.

--- lexical-core/build.rs
+++ lexical-core/build.rs
@@ -7,14 +7,12 @@ fn main() {
     // See `lexical-core/src/atof/algorithm/math.rs` for detailed
     // instructions of architecture instruction support for 64-bit
     // mathematical operations.
+
+    // https://github.com/rust-lang/cargo/issues/4302#issuecomment-316482399
+    let limb_64_archs = ["aarch64", "mips64", "powerpc64", "x86_64"];
     let limb_width_64 = match std::env::var("CARGO_CFG_TARGET_ARCH") {
-        Ok(arch) => ["aarch64", "mips64", "powerpc64", "x86_64"].contains(&&*arch),
-        Err(_) => cfg!(any(
-            target_arch = "aarch64",
-            target_arch = "mips64",
-            target_arch = "powerpc64",
-            target_arch = "x86_64"
-        ))
+        Ok(arch) => limb_64_archs.contains(&&*arch),
+        _ => false,
     };
     if limb_width_64 {
         println!("cargo:rustc-cfg=limb_width_64");
