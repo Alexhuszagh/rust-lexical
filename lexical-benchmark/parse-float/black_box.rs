25e9d216a125e4b36941bdb6b0060e77f721e962
Alex Huszagh <ahuszagh@gmail.com>
2021-08-31 18:25:47 -0500

    Bug fix for lints with LLVM assembly.

--- lexical-benchmark/parse-float/black_box.rs
+++ lexical-benchmark/parse-float/black_box.rs
@@ -1,3 +1,5 @@
+// Allow deprecated due to deprecation of LLVM assembly.
+#[allow(deprecated)]
 pub fn black_box<T>(mut dummy: T) -> T {
     // SAFETY: the inline assembly is a no-op.
     unsafe { llvm_asm!("" : : "r"(&mut dummy) : "memory" : "volatile") };
